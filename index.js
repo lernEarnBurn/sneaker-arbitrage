da7dea6f1a200180b2e4decc7bb90ebcc6f2da60
onetimecircle <lerners124@gmail.com>
2023-03-26 18:42:44 -0400

    made so deals.json waits for all promises

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ const nike = require('./nike.js')
 const fs = require('fs')
 
 
-function fileExists(file){
-    try{
-        fs.accessSync(file, fs.constants.F_OKs)
-        return true
-    }catch(err){
-        return false
-    }
-}
-
+let arbitrage = []
 
 async function main(){
     if(fileExists('nike.json')){
@@ -21,7 +13,7 @@ async function main(){
         await nike.getNikeShoes()
     }
 
-    let arbitrage = []
+   
 
     if(fileExists('deals.json')){
         fs.unlink('deals.json', (err) => {
@@ -30,28 +22,41 @@ async function main(){
           });
     }
     
+    /* Now that i am keeping track of promises I may be able to use
+       these to count all  browsers open and control the amount of them */
+    let promises = []
+
+    //code to choose how many browsers can open at once
     fs.readFile('./nike.json', (err, data) => {
         if (err){console.log(err)}
 
         let nikeJson = JSON.parse(data)
 
         for(let i = 0; i < nikeJson.length; i++){
-            stock.checkSneaker(nikeJson[i]).then(function(stockPrice) {
-                console.log(stockPrice) 
+            promises.push(stock.checkSneaker(nikeJson[i]).then(function(stockPrice) { 
+                console.log(`nike price: ${nikeJson[i]['price']}`)
                 addShoe(nikeJson[i], stockPrice, arbitrage)
             }).catch(function(error) {
                 console.log(error);
-            });
+            }));
+            
         }
-        fs.writeFileSync('deals.json', JSON.stringify(arbitrage)) 
-    }) 
 
+        Promise.all(promises).then(() => {
+            console.log(arbitrage)
+            fs.writeFileSync('deals.json', JSON.stringify(arbitrage))
+        })
+         
+    })
+
+    
     
 }
 
 
 function addShoe(shoe, stockPrice, arbitrage){
     if(isOppurtunity(shoe['price'], stockPrice)){
+        console.log('true')
         const analysis = {
             'name' : shoe['name'],
             'nike_price' : '$' + shoe['price'],
@@ -61,6 +66,7 @@ function addShoe(shoe, stockPrice, arbitrage){
 
         arbitrage.push(analysis)
     }
+
 
 }
 
@@ -73,4 +79,17 @@ function isOppurtunity(nikePrice, stockPrice){
 }
 
 
-main()
+function fileExists(file){
+    try{
+        fs.accessSync(file, fs.constants.F_OKs)
+        return true
+    }catch(err){
+        return false
+    }
+}
+
+
+if (require.main === module) {
+    main() 
+}
+
